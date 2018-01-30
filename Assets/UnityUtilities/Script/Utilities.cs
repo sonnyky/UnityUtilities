0a605d81588a60b7de1d0b167b9821987fc71d6b
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-01-30 18:40:45 +0900

    Initial commit

---
+++ Assets/UnityUtilities/Script/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tinker
+{
+    internal static class Utilities
+    {
+        private static JavaScriptSerializer json;
+        private static JavaScriptSerializer JSON { get { return json ?? (json = new JavaScriptSerializer()); } }
+
+        public static Stream ToStream(this string @this)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(@this);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+
+        public static T ParseXML<T>(this string @this) where T : class
+        {
+            var reader = XmlReader.Create(@this.Trim().ToStream(), new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Document });
+            return new XmlSerializer(typeof(T)).Deserialize(reader) as T;
+        }
+
+        public static T ParseJSON<T>(this string @this) where T : class
+        {
+            return JSON.Deserialize<T>(@this.Trim());
+        }
+    }
+}
