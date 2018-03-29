b3b6b64849680eb5cb01a91ae8c7424d2304bda2
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-03-29 19:19:24 +0900

    OSC
    Added sample scene to receive data from OSC.
    Added dependencies : UniRX, UnityOSC packages.

---
+++ Assets/UnityUtilities/Scripts/UDPReceiverRx/UdpReceiverRx.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using UniRx;
+using UdpReceiverUniRx;
+using UnityOSC;
+using System;
+
+public class UdpReceiverRx : MonoBehaviour
+{
+    private const int listenPort = 5001;
+    private static UdpClient myClient;
+    private bool isAppQuitting;
+    public IObservable<UdpState> _udpSequence;
+
+    private bool dataAvailable = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _udpSequence = Observable.Create<UdpState>(observer =>
+        {
+            Debug.Log(string.Format("_udpSequence thread: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId));
+            try
+            {
+                myClient = new UdpClient(listenPort);
+            }
+            catch (SocketException ex)
+            {
+                observer.OnError(ex);
+            }
+            IPEndPoint remoteEP = null;
+            myClient.EnableBroadcast = true;
+            myClient.Client.ReceiveTimeout = 5000;
+            while (!isAppQuitting)
+            {
+                try
+                {
+                    remoteEP = null;
+                    var receivedMsg = System.Text.Encoding.ASCII.GetString(myClient.Receive(ref remoteEP));
+                    observer.OnNext(new UdpState(remoteEP, receivedMsg));
+                    dataAvailable = true;
+
+                }
+                catch (SocketException)
+                {
+                    dataAvailable = false;
+                    //Debug.Log("UDP::Receive timeout");
+                }
+            }
+            observer.OnCompleted();
+            return null;
+        })
+            .SubscribeOn(Scheduler.ThreadPool)
+            .Publish()
+            .RefCount();
+    }
+
+    void OnApplicationQuit()
+    {
+        isAppQuitting = true;
+        myClient.Client.Blocking = false;
+    }
+    public bool CheckDataAvailability()
+    {
+        return dataAvailable;
+    }
+
+}
