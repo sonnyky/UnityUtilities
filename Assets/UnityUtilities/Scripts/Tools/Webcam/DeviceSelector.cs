8f10bb0beaf55e5426dd4e94acc616e6bc4f261e
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-04-07 01:19:15 +0900

    add method to check max resolution supported by the device

--- Assets/UnityUtilities/Scripts/Tools/Webcam/DeviceSelector.cs
+++ Assets/UnityUtilities/Scripts/Tools/Webcam/DeviceSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class DeviceSelector : MonoBehaviour
@@ -9,13 +10,25 @@ public class DeviceSelector : MonoBehaviour
     public UnityEvent m_DevicesReadyEvent;
 
     private WebCamDevice[] devices;
+    private List<WebCamTexture> texs;
+
+    public RawImage m_Screen;
+
+    public struct Resolution
+    {
+        public int height;
+        public int width;
+    }
 
     // Use this for initialization
     void Start()
     {
+        texs = new List<WebCamTexture>();
         devices = WebCamTexture.devices;
-        for (int i = 0; i < devices.Length; i++)
-            Debug.Log(devices[i].name);
+        if (devices.Length > 0)
+        {
+            GetDeviceMaxResolution();
+        }
 
         m_DevicesReadyEvent.Invoke();
     }
@@ -23,6 +36,23 @@ public class DeviceSelector : MonoBehaviour
     public int GetNumberOfDevices()
     {
         return devices.Length;
+    }
+
+    public Resolution[] GetDeviceMaxResolution()
+    {
+        Resolution[] res = new Resolution[devices.Length];
+        for(int i=0; i< devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+            WebCamTexture tex = new WebCamTexture(devices[i].name, 4096, 4096, 60);
+            texs.Add(tex);
+            texs[i].Play();
+            Debug.Log("tex" + i + " height and width : " + texs[i].height + ", " + texs[i].width);
+            res[i].height = texs[i].height;
+            res[i].width = texs[i].width;
+            texs[i].Stop();
+        }
+        return res;
     }
 
     public WebCamDevice GetDevice(int id)
@@ -35,5 +65,5 @@ public class DeviceSelector : MonoBehaviour
         }
         return devices[id];
     }
-
+    
 }
